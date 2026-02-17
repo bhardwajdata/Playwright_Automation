5d7b61d31e1380ca3fc0e9dbfbc66ef6400d6547
Suraj Bhardwaj <suraj.bhardwaj@ad.mindbowser.com>
2026-02-17 15:21:14 +0530

    CORN update

--- PlaywrightNPM/tests/runner.spec.js
+++ PlaywrightNPM/tests/runner.spec.js
@@ -3,6 +3,8 @@
 // We have also compared execution time in chromium vs firefox vs webkit. Execution time is more in firefox and webkit as compared to chromium. And also few tests are failing in webkit as compared to chromium and firefox. This is because of the view port size.
 // More workers, more CPU usage. But the execution time is less. So we have to decide whether we want to reduce execution time or reduce CPU usage. If we want to reduce execution time, we can increase the number of workers. 
 // For CI we can keep the number of workers less to reduce CPU usage. For local execution we can keep the number of workers more to reduce execution time.
+// In workflow file, we have to install only chromium browser to reduce the execution time. If we install all browsers, it will take more time. If we want to test in all browsers, we can install all browsers. 
+
 
 import '../tests/RegressionTest/Hub.spec.js';
 import '../tests/RegressionTest/baba.spec.js';
